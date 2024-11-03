2b97c8111b1478642a4b3b48bf2c408d7a73b349
James Teh <jamie@jantrid.net>
2024-11-03 10:44:05 +1000

    flood: Log the battery voltage when it is reported.

--- tehome/flood.py
+++ tehome/flood.py
@@ -8,5 +8,6 @@ async def onFloodSensor():
 	flood = request.args.get("flood")
 	await homebridge.updateChar(ACC, "LeakDetected", int(flood))
 	batV = float(request.args.get("batV"))
+	print(f"flood batV {batV}")
 	await homebridge.updateChar(ACC, "StatusLowBattery", 1 if batV < 2.3 else 0)
 	return ""
