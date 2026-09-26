jcsteh/tehome

commit 7265c4206a70cc9fe9079dc6f17068c7a106fe0a

Add doorbell notifications.

tehome/__main__.py
@@ -1,5 +1,5 @@
 import asyncio
-from . import homebridge, airtouch, phone, web, homekit, energyUi, garage
+from . import homebridge, airtouch, phone, web, homekit, energyUi, garage, doorbell
 
 async def main():
 	await asyncio.gather(
@@ -9,6 +9,7 @@ async def main():
 		web.handler(),
 		homekit.poll(),
 		garage.batteryChecker(),
+		doorbell.handler(),
 	)
 
 if __name__ == "__main__":

tehome/doorbell.py
@@ -1,0 +1,33 @@
+import asyncio
+import datetime
+import json
+from google.cloud import pubsub_v1
+from . import config, homebridge
+
+CHIME_ACC = "Doorbell"
+
+def callback(message):
+	payload = json.loads(message.data.decode("utf-8"))
+	message.ack()
+	if payload.get("eventThreadState", "STARTED") != "STARTED":
+		return
+	events = payload.get("resourceUpdate", {}).get("events")
+	if not events:
+		return
+	if events.get("sdm.devices.events.DoorbellChime.Chime"):
+		print("Doorbell pressed")
+		now = datetime.datetime.now()
+		if config.DOORBELL_START_TIME <= (now.hour, now.minute) <= config.DOORBELL_END_TIME:
+			asyncio.run_coroutine_threadsafe(
+				homebridge.updateChar(CHIME_ACC, "ProgrammableSwitchEvent", 0),
+				loop
+			)
+	if events.get("sdm.devices.events.CameraPerson.Person"):
+		print("Doorbell: person detected")
+
+async def handler():
+	global loop
+	loop = asyncio.get_event_loop()
+	subscriber = pubsub_v1.SubscriberClient()
+	streamingPull = subscriber.subscribe(config.GOOGLE_SUBSCRIPTION, callback=callback)
+	await asyncio.to_thread(streamingPull.result)

tehome/homebridge.py
@@ -2,7 +2,7 @@ import os
 import json
 import asyncio
 import websockets
-from . import garage, airtouch, flood
+from . import garage, airtouch
 
 async def connect():
 	global bridge
@@ -71,5 +71,7 @@ async def setup():
 	await airtouch.airtouch.UpdateInfo()
 	for group in airtouch.airtouch.groups:
 		await addAccessory("%s%d" % (airtouch.ACC_PREFIX, group), "Thermostat")
+	from . import flood, doorbell, homekit
 	await addAccessory(flood.ACC, "LeakSensor")
 	await addAccessory(homekit.ACC, "StatelessProgrammableSwitch")
+	await addAccessory(doorbell.CHIME_ACC, "Doorbell")

commit dc429bf71ad7f9601c55a68f86dd19ea3f00d422

flood: Import at startup to fix broken sensor reporting. Regressed in 7265c420.

tehome/__main__.py
@@ -1,5 +1,7 @@
 import asyncio
-from . import homebridge, airtouch, web, homekit, energyUi, garage, doorbell
+from . import (
+	homebridge, airtouch, web, homekit, energyUi, garage, doorbell, flood,
+)
 
 async def main():
 	await asyncio.gather(